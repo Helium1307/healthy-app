0bcca553723f6c938c691fadb041b4d0a8cd1124
Hélio <heliofilho1307@hotmail.com>
2021-11-10 11:31:07 -0300

    teste 2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Salve</Text>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
     </View>
   );
